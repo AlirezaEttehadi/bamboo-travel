8ff13dd620b769449d5c12bb1709550fc145c254
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-03-04 17:52:45 +0330

    fix:header-layout

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ import { IconSearch } from "@tabler/icons-react";
 
 const Header = () => {
   return (
-    <header className="mx-auto lg:p-10 p-4 flex items-center justify-between">
+    <header className="mx-auto lg:p-10 p-4 grid grid-cols-3">
       <div className="flex items-center gap-4">
         <Link isBlock href="/planner" color="success">
           Trip Planner
@@ -17,22 +17,25 @@ const Header = () => {
           Wishlist
         </Link>
       </div>
-      <Link href="/">
-        <Image
-          src="/bamboo-travel-logo.png"
-          alt="Bamboo Travel Logo"
-          width={200}
-          height={200}
-          priority
-        />
-      </Link>
-      <div className="flex items-center gap-4">
+      <div className="flex justify-center">
+        <Link href="/">
+          <Image
+            src="/bamboo-travel-logo.png"
+            alt="Bamboo Travel Logo"
+            width={200}
+            height={200}
+            priority
+          />
+        </Link>
+      </div>
+      <div className="flex items-center justify-end gap-4">
         <Input
           type="text"
           placeholder="Search"
           startContent={
             <IconSearch className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
           }
+          className="max-w-fit"
         />
         <Button as={Link} variant="ghost" href="/login" color="success">
           Login
