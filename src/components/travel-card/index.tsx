9ff250658a6c957a4631663161a6088784b9f52c
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-03-04 19:08:53 +0330

    fix:travel-section

--- src/components/travel-card/index.tsx
+++ src/components/travel-card/index.tsx
@@ -8,8 +8,8 @@ const TravelCard = () => {
         alt="Card background"
         className="object-cover rounded-xl"
         src="/hotel.jpg"
-        width={400}
-        height={400}
+        width={800}
+        height={800}
       />
       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
         <p className="text-tiny text-white/80">Available soon.</p>
--- src/components/travel-list/index.tsx
+++ src/components/travel-list/index.tsx
@@ -4,7 +4,7 @@ import TravelCard from "../travel-card";
 
 const TravelList = () => {
   return (
-    <section className="mx-auto lg:p-16 p-4 flex items-center justify-between flex-wrap gap-8">
+    <section className="mx-auto lg:p-16 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map((item) => {
         return <TravelCard key={item} />;
       })}
