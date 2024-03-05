d11d28c68a5d2e84caac68ab0034da0cfa40d447
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-03-05 14:25:23 +0330

    refactor:travel-card-list

--- src/components/travel-card/index.tsx
+++ src/components/travel-card/index.tsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { FC } from "react";
 import { Card, Image, CardHeader, CardBody } from "@nextui-org/react";
 import NextImage from "next/image";
 import Link from "next/link";
 
 import Rating from "../rating";
 
-const TravelCard = () => {
+type Props = {
+  hotel: {
+    id: number;
+    name: string;
+    price: string;
+    rate: number;
+    image: string;
+  };
+};
+
+const TravelCard: FC<Props> = ({ hotel }) => {
   return (
-    <Card isPressable as={Link} href="/hotels/id">
+    <Card isPressable as={Link} href={`/hotels/${hotel.id}`}>
       <CardHeader className="py-2 px-4 flex-col items-start gap-2">
-        <h4 className="font-bold text-large">The Plaza Hotel</h4>
-        <div className="text-tiny uppercase font-bold">7,300 $</div>
-        <Rating rate={4} />
+        <h4 className="font-bold text-large">{hotel.name}</h4>
+        <div className="text-tiny uppercase font-bold">{hotel.price}</div>
+        <Rating rate={hotel.rate} />
       </CardHeader>
       <CardBody className="p-0">
         <Image
           alt="Card background"
           className="object-cover w-full rounded-xl"
-          src="/hotel.jpg"
+          src={hotel.image}
           width={600}
           height={500}
           as={NextImage}
--- src/components/travel-list/index.tsx
+++ src/components/travel-list/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import { hotels } from "@bamboo/data/hotels";
+
 import TravelCard from "../travel-card";
 
 const TravelList = () => {
   return (
     <section className="mx-auto lg:p-16 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map((item) => {
-        return <TravelCard key={item} />;
+      {hotels.map((hotel) => {
+        return <TravelCard key={hotel.id} hotel={hotel} />;
       })}
     </section>
   );
