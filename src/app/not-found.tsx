67d80876dfc402b0cb0b56d4badaffbcedcb2651
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-03-05 12:56:31 +0330

    fix:not-found-page-responsiveness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex-grow my-36 flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="text-center">
+    <main className="flex-grow my-36 lg:my-52 flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="text-center space-y-8">
+        <h1 className="lg:text-4xl text-xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+          Page not found.
+        </h1>
         <p className="text-sm font-semibold text-green-600 uppercase tracking-wide">
           404 error
         </p>
-        <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-          Page not found.
-        </h1>
-        <p className="mt-2 text-base text-gray-500">
+        <p className="text-base text-gray-500">
           Sorry, we couldn’t find the page you’re looking for.
         </p>
         <Button
