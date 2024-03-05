d785f95395ad0ec0bcb1cd2012fdff45fa3d3aca
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-03-05 15:10:43 +0330

    chore:add-highlights-to-data

--- src/data/hotels.ts
+++ src/data/hotels.ts
@@ -5,6 +5,32 @@ export const hotels = [
     price: "7,300 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Guests can enjoy international cuisine at the on-site restaurant.`,
+      },
+      {
+        id: 2,
+        description: `There is a hairdresser's, Hammam, a spa and wellness centre at the property.`,
+      },
+      {
+        id: 3,
+        description: `Couples particularly like the location — they rated it 8.7 for a two-person trip.`,
+      },
+      {
+        id: 4,
+        description: `All rooms have a TV.`,
+      },
+      {
+        id: 5,
+        description: `Offering an outdoor pool and sun terrace, Hôtel Racine is located in Marrakech, a 5-minute drive from Djemaa El Fna square. The property offers a fitness centre and a free Wi-Fi access in all areas.`,
+      },
+      {
+        id: 6,
+        description: `Marrakech Plaza is a 5-minute walk from Hôtel Racine, while Carré Eden Shopping Center is a 10-minute walk away. The nearest airport is Marrakech-Menara Airport, 4 km from the property.`,
+      },
+    ],
   },
   {
     id: 2,
@@ -12,6 +38,28 @@ export const hotels = [
     price: "8,500 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience opulent luxury with personalized service at Ritz-Carlton.`,
+      },
+      {
+        id: 2,
+        description: `Indulge in world-class dining with a diverse menu curated by renowned chefs.`,
+      },
+      {
+        id: 3,
+        description: `Unwind in the lavish spa offering exclusive treatments and wellness programs.`,
+      },
+      {
+        id: 4,
+        description: `Panoramic views of the city skyline and beyond from your elegantly appointed room.`,
+      },
+      {
+        id: 5,
+        description: `Conveniently located near major attractions, making it an ideal choice for leisure and business.`,
+      },
+    ],
   },
   {
     id: 3,
@@ -19,6 +67,28 @@ export const hotels = [
     price: "9,200 $",
     rate: 3,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Discover a harmonious blend of luxury and tranquility at Four Seasons Hotel.`,
+      },
+      {
+        id: 2,
+        description: `Immerse yourself in the lush surroundings of the hotel's beautifully landscaped gardens.`,
+      },
+      {
+        id: 3,
+        description: `Exceptional dining experiences with a focus on locally sourced, seasonal ingredients.`,
+      },
+      {
+        id: 4,
+        description: `Relax in spacious and elegantly designed suites with modern amenities.`,
+      },
+      {
+        id: 5,
+        description: `Unmatched hospitality and personalized services to make your stay truly memorable.`,
+      },
+    ],
   },
   {
     id: 4,
@@ -26,6 +96,28 @@ export const hotels = [
     price: "8,000 $",
     rate: 2,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Immerse yourself in the opulence of Mandarin Oriental's oriental-inspired design.`,
+      },
+      {
+        id: 2,
+        description: `Dine at the award-winning restaurants offering a fusion of Asian and international cuisines.`,
+      },
+      {
+        id: 3,
+        description: `Experience ultimate relaxation at the luxurious spa with holistic wellness treatments.`,
+      },
+      {
+        id: 4,
+        description: `Breathtaking views of the cityscape and serene surroundings from your stylish room.`,
+      },
+      {
+        id: 5,
+        description: `Strategically located near major attractions for a perfect blend of leisure and business.`,
+      },
+    ],
   },
   {
     id: 5,
@@ -33,6 +125,28 @@ export const hotels = [
     price: "7,800 $",
     rate: 4,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience the epitome of timeless luxury at Waldorf Astoria.`,
+      },
+      {
+        id: 2,
+        description: `Indulge in the culinary delights of world-class chefs at the hotel's signature restaurants.`,
+      },
+      {
+        id: 3,
+        description: `Relax and rejuvenate at the exclusive spa, offering a range of bespoke treatments.`,
+      },
+      {
+        id: 4,
+        description: `Elegantly designed rooms with a perfect blend of comfort and sophistication.`,
+      },
+      {
+        id: 5,
+        description: `Centrally located, providing easy access to cultural landmarks and entertainment.`,
+      },
+    ],
   },
   {
     id: 6,
@@ -40,6 +154,28 @@ export const hotels = [
     price: "7,000 $",
     rate: 4,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience a seamless blend of modern luxury and classic elegance at InterContinental.`,
+      },
+      {
+        id: 2,
+        description: `Indulge your taste buds with a diverse range of international cuisines at the hotel's restaurants.`,
+      },
+      {
+        id: 3,
+        description: `Unwind and recharge at the state-of-the-art fitness center and spa facilities.`,
+      },
+      {
+        id: 4,
+        description: `Enjoy stunning city views from your well-appointed and comfortable room.`,
+      },
+      {
+        id: 5,
+        description: `Strategically located for both business and leisure travelers, with easy access to key destinations.`,
+      },
+    ],
   },
   {
     id: 7,
@@ -47,6 +183,28 @@ export const hotels = [
     price: "7,600 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Immerse yourself in the grandeur of Fairmont Hotel, known for its iconic architecture.`,
+      },
+      {
+        id: 2,
+        description: `Savor exquisite culinary creations crafted by renowned chefs at the hotel's restaurants.`,
+      },
+      {
+        id: 3,
+        description: `Rejuvenate your senses with indulgent spa treatments in a serene and luxurious setting.`,
+      },
+      {
+        id: 4,
+        description: `Luxurious and spacious rooms designed for the utmost comfort and relaxation.`,
+      },
+      {
+        id: 5,
+        description: `Centrally located, providing easy access to cultural landmarks, shopping, and entertainment.`,
+      },
+    ],
   },
   {
     id: 8,
@@ -54,6 +212,28 @@ export const hotels = [
     price: "6,900 $",
     rate: 4,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Discover contemporary luxury and warm hospitality at Hyatt Regency.`,
+      },
+      {
+        id: 2,
+        description: `Savor a diverse range of cuisines at the hotel's restaurants, each offering a unique dining experience.`,
+      },
+      {
+        id: 3,
+        description: `Unwind in the stylish and well-equipped rooms designed for both comfort and productivity.`,
+      },
+      {
+        id: 4,
+        description: `Stay active and refreshed with the fitness center, spa, and other recreational facilities.`,
+      },
+      {
+        id: 5,
+        description: `Conveniently located near major business and leisure hubs for a seamless stay.`,
+      },
+    ],
   },
   {
     id: 9,
@@ -61,6 +241,28 @@ export const hotels = [
     price: "6,800 $",
     rate: 2,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience the renowned Hilton hospitality and comfort at Hilton Hotel.`,
+      },
+      {
+        id: 2,
+        description: `Dine in style at the hotel's restaurants, offering a variety of culinary delights.`,
+      },
+      {
+        id: 3,
+        description: `Relax and rejuvenate with a range of spa treatments and wellness activities.`,
+      },
+      {
+        id: 4,
+        description: `Enjoy spacious and well-appointed rooms, perfect for a restful stay.`,
+      },
+      {
+        id: 5,
+        description: `Conveniently situated for both business and leisure travelers with easy access to key attractions.`,
+      },
+    ],
   },
   {
     id: 10,
@@ -68,6 +270,28 @@ export const hotels = [
     price: "6,500 $",
     rate: 3,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience the warm hospitality and contemporary comfort at Sheraton Hotel.`,
+      },
+      {
+        id: 2,
+        description: `Indulge your palate with a variety of culinary delights at the hotel's dining establishments.`,
+      },
+      {
+        id: 3,
+        description: `Recharge and rejuvenate at the modern fitness center and spa facilities.`,
+      },
+      {
+        id: 4,
+        description: `Enjoy well-designed rooms with a focus on both style and functionality.`,
+      },
+      {
+        id: 5,
+        description: `Centrally located for easy access to cultural attractions, shopping, and entertainment.`,
+      },
+    ],
   },
   {
     id: 11,
@@ -75,6 +299,28 @@ export const hotels = [
     price: "7,000 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience the unparalleled luxury and world-class service at Marriott Hotel.`,
+      },
+      {
+        id: 2,
+        description: `Savor delectable cuisine crafted by top chefs in the hotel's signature restaurants.`,
+      },
+      {
+        id: 3,
+        description: `Rejuvenate your senses at the state-of-the-art spa, offering a range of indulgent treatments.`,
+      },
+      {
+        id: 4,
+        description: `Luxurious rooms designed for both relaxation and productivity, ensuring a memorable stay.`,
+      },
+      {
+        id: 5,
+        description: `Prime location for business and leisure travelers, with easy access to key destinations.`,
+      },
+    ],
   },
   {
     id: 12,
@@ -82,6 +328,28 @@ export const hotels = [
     price: "6,700 $",
     rate: 2,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Discover a rejuvenating stay with a focus on wellness at Westin Hotel.`,
+      },
+      {
+        id: 2,
+        description: `Savor wholesome and nutritious cuisine at the hotel's dining venues.`,
+      },
+      {
+        id: 3,
+        description: `Recharge your body and mind at the wellness-focused facilities, including a fitness center and spa.`,
+      },
+      {
+        id: 4,
+        description: `Well-appointed rooms designed for a restful and rejuvenating experience.`,
+      },
+      {
+        id: 5,
+        description: `Convenient location for those seeking a balance between business and well-being.`,
+      },
+    ],
   },
   {
     id: 13,
@@ -89,6 +357,28 @@ export const hotels = [
     price: "6,300 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience modern comfort and Hilton's signature hospitality at Hilton Garden Inn.`,
+      },
+      {
+        id: 2,
+        description: `Indulge in delicious meals at the hotel's dining options, featuring a variety of culinary delights.`,
+      },
+      {
+        id: 3,
+        description: `Stay productive and energized with the well-equipped fitness center and convenient amenities.`,
+      },
+      {
+        id: 4,
+        description: `Relax in cozy and stylish rooms designed for a comfortable and enjoyable stay.`,
+      },
+      {
+        id: 5,
+        description: `Ideal location for both business and leisure travelers, with easy access to key attractions.`,
+      },
+    ],
   },
   {
     id: 14,
@@ -96,6 +386,28 @@ export const hotels = [
     price: "6,000 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience a welcoming and relaxed atmosphere at Holiday Inn.`,
+      },
+      {
+        id: 2,
+        description: `Enjoy diverse dining options with a focus on quality and taste at the hotel's restaurants.`,
+      },
+      {
+        id: 3,
+        description: `Unwind and recharge at the leisure facilities, including a refreshing pool and fitness center.`,
+      },
+      {
+        id: 4,
+        description: `Comfortable and well-appointed rooms for a pleasant and enjoyable stay.`,
+      },
+      {
+        id: 5,
+        description: `Conveniently located for both business and leisure travelers, with easy access to attractions.`,
+      },
+    ],
   },
   {
     id: 15,
@@ -103,6 +415,28 @@ export const hotels = [
     price: "5,800 $",
     rate: 5,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience the vibrant and contemporary ambiance at Radisson Blu.`,
+      },
+      {
+        id: 2,
+        description: `Indulge in a culinary journey with the diverse and flavorful options at the hotel's dining venues.`,
+      },
+      {
+        id: 3,
+        description: `Relax and rejuvenate at the modern spa and wellness facilities for a truly refreshing experience.`,
+      },
+      {
+        id: 4,
+        description: `Contemporary and stylish rooms designed for both comfort and functionality.`,
+      },
+      {
+        id: 5,
+        description: `Centrally located for easy access to business hubs, shopping, and cultural attractions.`,
+      },
+    ],
   },
   {
     id: 16,
@@ -110,5 +444,27 @@ export const hotels = [
     price: "5,500 $",
     rate: 4,
     image: "/hotel.jpg",
+    highlights: [
+      {
+        id: 1,
+        description: `Experience contemporary and stylish accommodation at Novotel.`,
+      },
+      {
+        id: 2,
+        description: `Savor a variety of delicious cuisines at the hotel's dining options, catering to diverse tastes.`,
+      },
+      {
+        id: 3,
+        description: `Stay active and refreshed with the modern fitness center and recreational amenities.`,
+      },
+      {
+        id: 4,
+        description: `Comfortable and well-designed rooms, ensuring a relaxing and enjoyable stay.`,
+      },
+      {
+        id: 5,
+        description: `Convenient location for both business and leisure travelers, with easy access to key destinations.`,
+      },
+    ],
   },
 ] as const;
